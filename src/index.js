b2c1a01df17cda4ae8174f3d520c2521e57fb7cc
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2019-08-06 09:49:48 +0800

    task: update polling interval to 1.5m

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import {checkSystemMemory} from './checkSystemMemory'
-import {trackIt} from './trackIt'
-import daemonizeProcess from 'daemonize-process'
+import { checkSystemMemory } from "./checkSystemMemory";
+import { trackIt } from "./trackIt";
+import daemonizeProcess from "daemonize-process";
 
-const pollIntervalMs = 10000
+const pollIntervalMs = 90000;
 
 const mainThread = async () => {
-
   // Record user active window
-  await trackIt()
+  await trackIt();
 
   // check RAM if it is about to be full. Then notify user if almost full
-  await checkSystemMemory()
+  await checkSystemMemory();
 
   // Re execute the main thread after some interval
-  await new Promise(resolve => setTimeout(resolve, pollIntervalMs))
-  await mainThread()
-}
+  await new Promise(resolve => setTimeout(resolve, pollIntervalMs));
+  await mainThread();
+};
 
-mainThread().then(() => console.log('LOCAL VCR STARTED'))
+mainThread().then(() => console.log("LOCAL VCR STARTED"));
 
 // Let process run in background
-daemonizeProcess()
+daemonizeProcess();
